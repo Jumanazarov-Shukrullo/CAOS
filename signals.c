ff2381b8be77fbb1ae4d3ffcdf6200229299a28e
Shukrullo <79999273+Jumanazarov-Shukrullo@users.noreply.github.com>
2022-12-05 14:01:50 +0500

    Update signals.c

--- signals.c
+++ signals.c
@@ -19,25 +19,23 @@ void handle_signal(int signum, siginfo_t* info, void* extra) {
         sigqueue(pid,SIGRTMIN + signum,  (union sigval) {.sival_int = value}); 
 } 
  
-void handle_sigterm (int signum) { 
- state = 3; 
-} 
+void handle_sigterm (int signum) {state = 3;} 
  
 int main(int argc, char* argv[]) { 
-        struct sigaction sig_action; 
-        memset(&sig_action, 0, sizeof(sig_action)); 
-        sig_action.sa_sigaction = handle_signal; 
-        sig_action.sa_flags = SA_SIGINFO; 
-        sigaction(SIGRTMIN, &sig_action, NULL); 
-        struct sigaction action_term; 
-        memset(&action_term, 0, sizeof(action_term)); 
-        action_term.sa_handler = handle_sigterm; 
-        action_term.sa_flags = SA_RESTART; 
-        sigaction(SIGTERM, &action_term, NULL); 
-        printf("pid: %d", getpid()); 
-        fflush(stdout); 
-        while(true) { 
-  if (state == 3) 
+ struct sigaction sig_action; 
+ memset(&sig_action, 0, sizeof(sig_action)); 
+ sig_action.sa_sigaction = handle_signal; 
+ sig_action.sa_flags = SA_SIGINFO; 
+ sigaction(SIGRTMIN, &sig_action, NULL); 
+ struct sigaction action_term; 
+ memset(&action_term, 0, sizeof(action_term)); 
+ action_term.sa_handler = handle_sigterm; 
+ action_term.sa_flags = SA_RESTART;
+ sigaction(SIGTERM, &action_term, NULL);
+ printf("pid: %d", getpid()); 
+ fflush(stdout); 
+ while(true) { 
+   if (state == 3) 
    break; 
  } 
 }
